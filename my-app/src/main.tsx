4d53927db710401b958f53d0be753faf2a65f508
Matteo Hernandez <matteo.hernandez.1@ulaval.ca>
2026-07-07 18:42:33 -0400

    basic

--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -1,11 +1,23 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes.tsx'
+import { Provider } from 'react-redux'
+import { store } from './store.ts'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <RouterProvider router={router}/>
-  </StrictMode>,
-)
+const container = document.getElementById('root');
+
+if(container){
+  const root = createRoot(container);
+  root.render(
+    <Provider store={store}>
+      <RouterProvider router={router}/>
+    </Provider>
+  )
+}
+else {
+  throw new Error(
+    "root not found"
+  )
+}
+
--- my-app/src/pages /my-devices/my-devices.tsx
+++ my-app/src/pages /my-devices/my-devices.tsx
@@ -1,6 +1,28 @@
-export default function MyDevices() {
+import { useDispatch, useSelector } from "react-redux"
+import type { RootState } from "../../store"
+import { decrement, increment } from "../../reducers/counterSlice";
 
+export default function MyDevices() {
+    const count = useSelector((state: RootState) => state.counter.value);
+    const dispatch =  useDispatch();
     return (
-        <h2>My Devices</h2>
+    <div>
+        <h2>Devices</h2>
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
     )
 }
